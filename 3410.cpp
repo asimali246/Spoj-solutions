1f01df316ce11e2f42079080fe7be8f69a29da11
asimali246 <asimali246@yahoo.com>
2014-03-04 14:49:47 +0530

    Create 3410.cpp

---
+++ 3410.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 2147483647
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> llp;
+typedef pair<double, double> dd;
+typedef pair<char, int> ci;
+
+int n;
+int ans[111];
+
+int main()
+{
+    for(int i=1;i<=100;++i)
+        ans[i]=ans[i-1]+(i*i);
+    while(1)
+    {
+        scanf("%d", &n);
+        if(!n)
+            break;
+        printf("%d\n", ans[n]);
+    }
+    return 0;
+}
