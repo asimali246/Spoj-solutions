240cf3f0de0551e6edfcb4940d94d3d06a606e27
asimali246 <asimali246@yahoo.com>
2014-03-04 19:02:23 +0530

    Create 4300.cpp

---
+++ 4300.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 2147483647
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> llp;
+typedef pair<double, double> dd;
+typedef pair<char, int> ci;
+
+int n, ans;
+
+int main()
+{
+    scanf("%d", &n);
+    for(int i=1;i<=n;++i)
+    {
+        for(int j=i;j<=n;++j)
+            if(i*j<=n)++ans;
+            else
+                break;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
