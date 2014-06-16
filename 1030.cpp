ea45b1aca8f9fac2d1f34f205ee79d601b8086a3
asimali246 <asimali246@yahoo.com>
2014-06-17 00:17:42 +0530

    Create 1030.cpp

---
+++ 1030.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int t;
+ll x;
+
+int main(){
+    scanf("%d", &t);
+    while(t--){
+        scanf("%lld", &x);
+        printf("%lld\n", (ll)192+(x-1)*250);
+    }
+    return 0;
+}
