f79f3b3819787a3147be8b2ae042a415545ea482
asimali246 <asimali246@yahoo.com>
2014-06-17 22:51:16 +0530

    Create 7424.cpp

---
+++ 7424.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int a, b;
+
+int main(){
+    while(1){
+        scanf("%d %d", &a, &b);
+        if(a==-1 && b==-1)
+            break;
+        if(a>b)
+            swap(a, b);
+        printf("%d\n", (b/(a+1))+(b%(a+1)?1:0));
+    }
+    return 0;
+}
