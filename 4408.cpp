e6aefc17b3c8869995c7fcd1ed789fc5c0fd0481
asimali246 <asimali246@yahoo.com>
2014-06-17 22:20:52 +0530

    Create 4408.cpp

---
+++ 4408.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int L;
+double ans;
+
+int main(){
+    while(1){
+        scanf("%d", &L);
+        if(!L)
+            break;
+        ans=(double)(L*L)/(2.0*PI);
+        printf("%.2lf\n", ans);
+    }
+    return 0;
+}
