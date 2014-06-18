452aad31ab64493cb1063c0a834ec8cf9eb997a3
asimali246 <asimali246@yahoo.com>
2014-06-18 17:22:17 +0530

    Update 1163.cpp

--- 1163.cpp
+++ 1163.cpp
@@ -30,18 +30,28 @@ template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
-char s[111], i, a, b, l;
+char s[111], i, a, b, l, p;
 
 int main(){
     while(scanf("%s", s)!=EOF){
         l=(int)strlen(s);
-        a=b=0;
+        a=b=p=0;
         for(i=0;i<l;++i)
+            if(i==l-1 && (s[i]=='_'))
+                a=b=1;
+            else
+            if(i==0 && (s[i]=='_' || isupper(s[i])))
+                a=b=1;
+            if(s[i]=='_' && p)
+                a=b=1;
+            else
             if(s[i]=='_')
-                a=1;
+                a=1, p=1;
             else
             if(isupper(s[i]))
-                b=1;
+                b=1, p=0;
+            else
+                p=0;
         if(a && b)
             puts("Error!");
         else{
