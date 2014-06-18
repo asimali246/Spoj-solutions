b1cfb5a525e3f70510724f31de026a28fc3b4486
asimali246 <asimali246@yahoo.com>
2014-06-18 19:18:28 +0530

    Create 1028.cpp

---
+++ 1028.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int t, n, s;
+
+int main(){
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d %d", &n, &s);
+        if(s==0)
+            puts("Airborne wins.");
+        else
+            puts("Pagfloyd wins.");
+    }
+    return 0;
+}
