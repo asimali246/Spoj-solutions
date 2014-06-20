bf49ab0aee22c94739a28e227e8d35548eef3fad
asimali246 <asimali246@yahoo.com>
2014-06-20 16:12:24 +0530

    Create 97.cpp

---
+++ 97.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int b, n, w[111], p[111], dp[111][510], i, j, c, f;
+
+int main(){
+    while(1){
+        scanf("%d %d", &b, &n);
+        if(!b && !n)
+            break;
+        for(i=1;i<=n;++i)
+            scanf("%d %d", &w[i], &p[i]);
+        for(i=1;i<=n;++i)
+            for(j=1;j<=b;++j){
+                if(j>=w[i])
+                    dp[i][j]=max(dp[i-1][j], p[i]+dp[i-1][j-w[i]]);
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        c=f=-1;
+        for(i=1;i<=b;++i)
+            if(dp[n][i]>f)
+                f=dp[n][i], c=i;
+        printf("%d %d\n", c, f);
+    }
+    return 0;
+}
