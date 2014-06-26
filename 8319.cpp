ffd8837cb17ef07ffc68d581bd2f8ad3a49fc415
asimali246 <asimali246@yahoo.com>
2014-06-26 22:02:29 +0530

    Create 8319.cpp

---
+++ 8319.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 2147483647
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int n, ans, i, flag;
+
+int main(){
+    for(i=1;i<=10;++i){
+        scanf("%d", &n);
+        if(abs(ans+n-100)<=abs(ans-100) && !flag)
+            ans+=n;
+        else
+            flag=1;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
