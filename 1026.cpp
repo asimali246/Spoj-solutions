84de5e3ba20907cb3e37da00270aa4889944c83f
asimali246 <asimali246@yahoo.com>
2014-06-27 17:11:46 +0530

    Create 1026.cpp

---
+++ 1026.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 2147483647
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+double ans;
+int n, i, t;
+int main(){
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &n);
+        ans=0.0;
+        for(i=1;i<=n;++i)
+            ans+=(double)n/i;
+        printf("%.2lf\n", ans);
+    }
+    return 0;
+}
