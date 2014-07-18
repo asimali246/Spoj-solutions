3d8b44b9ee14b6a9583f360304e200f7a3ca6ead
asimali246 <asimali246@yahoo.com>
2014-07-18 23:44:00 +0530

    Create 106.cpp

---
+++ 106.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+
+#define ll long long int
+#define INF 2147483647
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int t, n, m;
+
+int main(){
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d %d", &n, &m);
+		printf("%d\n", ((n-m)&((m-1)/2))==0);
+	}
+	return 0;
+}
