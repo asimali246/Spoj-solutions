9b3b72dacd68e97f55f5d1a27df205b77559c52f
asimali246 <asimali246@yahoo.com>
2014-09-03 04:51:16 +0530

    Create 9952.cpp

---
+++ 9952.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <cassert>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+unsigned long long n, ans;
+
+int main() {
+	while( scanf("%llu", &n) != EOF ) {
+		ans = n / 9;
+		ans *= 81;
+		ans += (n%9) * ( n%9);
+		printf("%llu\n", ans);
+	}
+	return 0;
+}
