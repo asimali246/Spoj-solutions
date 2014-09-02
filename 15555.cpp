09c2975746b6cf59a9e34c0715cb873352e771a7
asimali246 <asimali246@yahoo.com>
2014-09-02 17:24:30 +0530

    Create 15555.cpp

---
+++ 15555.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <cassert>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int t;
+ll n, i;
+
+int main() {
+	scanf("%d", &t);
+	while( t-- ) {
+		scanf("%lld", &n);
+		for( i = n/2; ; --i ) {
+			if( gcd( n, i ) == 1 ) {
+				printf("%lld\n", i);
+				break;
+			}
+		}
+	}
+	return 0;
+}
