d12fd09356133be31260cabf521d7c9ffd0c9b15
asimali246 <asimali246@yahoo.com>
2014-09-10 22:47:31 +0530

    Create 359.cpp

---
+++ 359.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <cassert>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int t, n;
+char s[1<<17];
+
+int main() {
+	scanf("%d", &t);
+	while( t-- ) {
+		scanf("%d %s", &n, s);
+		putchar(s[(int)strlen(s) - 1]);
+		putchar('\n');
+	}
+	return 0;
+}
